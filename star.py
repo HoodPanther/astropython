4c8520169476b9a37e167c72991a48885e0498fb
Marco <marco.nazzari19@gmail.com>
2017-06-23 01:18:55 +0200

    Exercises 2 updated

--- star.py
+++ star.py
@@ -11,7 +11,6 @@ class Star:
     author: D. Andreasen
     """
     grav = 6.67384e-11  # Gravitational constant
-    au = 149597871  # average distance to the Sun from Earth
 
     def __init__(self, massin, radiusin):
         self.mass = massin
@@ -20,37 +19,12 @@ class Star:
     def get_surface_gravity(self):
         return Star.grav * self.mass / self.radius ** 2
 
-    def __str__(self):
-        # Fancy printing of a star
-        string = ''
-        string += '\n' + '*' * 33 + '\n'
-        string += '*** Welcome to the Star class ***\n'
-        string += '*' * 33 + '\n'
-        string += 'Mass is\t\t\t %.2f\n' % self.mass
-        string += 'Radius is\t\t %.2f\n' % self.radius
-        string += 'Surface gravity is\t %s\n' % self.get_surface_gravity()
-        return string
-
-    def __add__(self, other):
-        return Star(self.mass + other.mass, sqrt(self.radius + other.radius))
-
     @staticmethod
     def get_spectral_types():
         return ['O', 'B', 'A', 'F', 'G', 'K', 'M']
 
 
 def main():
-
-    # Create two stars
-    star1 = Star(1.1, 0.98)
-    star2 = Star(0.8, 1.3)
-
-    # Try to print them
-    print star1
-    print star2
-
-    # Now print the sum of the two stars
-    print star1 + star2
 
 
 if __name__ == '__main__':
